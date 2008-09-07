88ad1c73685f9aa7ea9f02d860ce01e06410f6b9
kristoffer <krille@mmx.(none)>
2008-09-07 03:11:59 +0200

    added comment

--- seq.c
+++ seq.c
@@ -1,4 +1,3 @@
-
 /*
   Copyright (C) 2004 Ian Esten
     
@@ -15,6 +14,8 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+  Testing git commit...
 */
 
 #include <jack/jack.h>
